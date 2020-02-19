91bb41034de60823123d3610f6c16d7f2b8a0386
lge6 <lia.eggleston@yale.edu>
2020-02-19 18:46:33 -0500

    statebutton

--- gui_test.py
+++ gui_test.py
@@ -10,6 +10,7 @@ ic = h.IClamp(seg)
 ic.amp = 40  # since by default a very large section
 ic.delay = 1
 ic.dur = 1
+is_checked = h.ref(1)
 
 h.xpanel("Fancier example")
 
@@ -23,6 +24,7 @@ h.xlabel("Here is some text")
 h.xvalue("h.t", "t")
 h.xbutton("finitialize", "h.finitialize(-65)")
 h.xbutton("run", "h.run()")
+h.xstatebutton("State", is_checked)
 
 vbox.intercept(False)
 vbox.map()
@@ -35,3 +37,22 @@ hbox.map()
 h.xpanel()
 
 h.finitialize(-65)
+
+"""Type "help", "copyright", "credits" or "license" for more information.
+>>> from neuron import h, gui
+>>> h.xpanel('leprechaun')
+0.0
+>>> n = h.ref(0)
+>>> def foo(*args):
+...     return
+... 
+>>> h.xvalue('how many', n, False, foo, True)
+0.0
+>>> h.xvalue('how masdf', n, False, foo, False)
+0.0
+>>> h.xpanel()
+0.0
+>>> import warnings
+>>> warnings.warn('nooooo')
+__main__:1: UserWarning: nooooo
+"""
