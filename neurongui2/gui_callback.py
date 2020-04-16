cdf0d3234404966a020de1380174023be3b0e131
Lia Eggleston <lia.eggleston@yale.edu>
2020-04-16 18:17:21 -0400

    more generalized redirection in gui_callback

--- neurongui2/gui_callback.py
+++ neurongui2/gui_callback.py
@@ -1,5 +1,6 @@
 from .gui import *
 import logging
+import warnings
 logging.basicConfig(level=logging.DEBUG, filename="mylog.txt")
 
 class Box:
@@ -47,26 +48,6 @@ class TextEditor:
     def name(self):
         return self.param
 
-class graphProto:
-    def __init__(self, *args):
-        self.expressions = []
-        self.mode = 0
-    def __repr__(self):
-        return 'GraphProto'
-    def addexpr(self, expr):
-        self.expressions.append(expr)
-        print(self.expressions)
-        return True
-    def plot(self, *args):
-        for e in self.expressions:
-            print(e+"\n")
-        return True
-
-def graphmode(mode):
-    print('obsolete graphmode')
-    print(mode)
-    return 0
-
 def list_browser(the_list):
     print('list_browser:')
     print(the_list)
@@ -85,6 +66,12 @@ fn_map = {
     'xvarlabel': xvarlabel
 }
 
+class_map = {
+    'HBox': HBox,
+    'VBox': VBox,
+    'Graph': Graph
+}
+
 def gui_callback(*args):
     global all_args
     all_args = args
@@ -94,35 +81,16 @@ def gui_callback(*args):
     params = args[3:]
     logging.debug("fn: %s", fn)
     logging.debug("obj: %r", obj)
-    logging.debug("params: %r", params)
-    if fn in fn_map:
-        return fn_map[fn](*params, context=context)
-
-    elif fn == 'VBox':
-        return VBox(*params)
-    elif fn == 'HBox':
-        return HBox(*params)
-    elif fn == 'Box.map':
-        return obj.map(*params)
-    elif fn == 'Box.intercept':
-        return obj.intercept(*params)
-
-    elif fn == 'List.browser':
+    logging.debug("params: %r", (params,))
+    
+    # testing
+    if fn == 'List.browser':
         return list_browser(obj)
 
     elif fn == 'TextEditor':
         return TextEditor(*params)
     elif fn == 'TextEditor.text':
         return obj.name()
-
-    elif fn == 'Graph':
-        return Graph(*params)
-    elif fn == 'Graph.addvar':
-        return obj.addvar(*params)
-
-    elif (fn == "doNotify") or (fn == "doEvents"):
-        print(fn)
-        return 0
 
     elif fn == 'graphmode':
         return graphmode(*params)
@@ -133,46 +101,18 @@ def gui_callback(*args):
         print('0:', params[2])
         print('1:', params[1])
         return float(input('your choice: '))
-    logging.debug("do something else (nothing given to do)")
+
+    # general redirection
+    try:
+        if not obj:
+            if fn in fn_map:
+                return fn_map[fn](*params, context=context)
+            elif fn in class_map:
+                return class_map[fn](*params)
+        else:
+            split = fn.split('.')
+            return getattr(obj, split[1])(*params)
+    except:
+        warnings.warn("redirection not set up for "+fn)
+
     return True
-
-"""
-DEBUG:root:fn: VBox
-DEBUG:root:obj: None
-DEBUG:root:params: ('Neuron',)
-DEBUG:root:fn: Box.ismapped
-DEBUG:root:obj: PyVBox
-DEBUG:root:params: ()
-DEBUG:root:fn: Box.map
-DEBUG:root:obj: PyVBox
-DEBUG:root:params: ()
-DEBUG:root:fn: Box.ismapped
-DEBUG:root:obj: <list_iterator object at 0x7f863f912fd0>
-DEBUG:root:params: ()
-
-
-# for testing
-elif fn == 'Graph':
-    return graphProto(*params)
-elif fn == 'Graph.addexpr':
-    return obj.addexpr(*params)
-elif fn == 'Graph.plot':
-    return obj.plot(*params)"""
-
-# real graph testing
-"""elif fn == 'Graph':
-    return Graph(*params)
-elif fn == 'Graph.addvar':
-    return obj.addvar(*params)"""
-
-# for testing purposes
-"""elif fn == 'VBox':
-    return Box('V', *params)
-elif fn == 'HBox':
-    return Box(*params)
-elif fn == 'Box.ismapped':
-    return obj.ismapped()
-elif fn == 'Box.map':
-    return obj.map(*params)
-elif fn == 'Box.unmap':
-    return obj.unmap(*params)"""
